45f9cc33b09c4a8e6b3ad432878641fc70394a8b
Shubham Singh <shubham88singh@gmail.com>
2020-09-09 01:53:04 +0530

    Adding the two pointer problem

---
+++ two-sum.js
@@ -0,0 +1,18 @@
+const targetPair = (arr, sum) => {
+  if (arr.length === 0) return -1;
+  let low = 0,
+    high = arr.length - 1;
+  while (low < high) {
+    let target = arr[low] + arr[high];
+    if (target < sum) {
+      low += 1;
+    } else if (target > sum) {
+      high -= 1;
+    } else {
+      return [low, high];
+    }
+  }
+  return -1;
+};
+
+console.log(targetPair([2,5,9,11],11));
