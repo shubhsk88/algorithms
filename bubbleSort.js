496036c1e23014f5eb1f19c12e6815d286a2004f
Shubham Singh <shubham88singh@gmail.com>
2020-09-09 21:35:08 +0530

    Adding the solution of bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([4, 3, 2, 5, 6, 4, 32]);
