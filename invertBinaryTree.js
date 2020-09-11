900ae8219a35b18425f6da2d9572c2be01630cf6
Shubham Singh <shubham88singh@gmail.com>
2020-09-11 17:12:16 +0530

    Adding the solution of the invert binary tree recursive solution

--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -1,3 +1,28 @@
+
+//Iterative approach
+var invertTree = function (root) {
+    let queue = [root];
+    while (queue.length) {
+      let currentNode = queue.shift();
+  
+      if (currentNode === null) {
+        continue;
+      }
+      swap(currentNode);
+      queue.push(currentNode.left);
+      queue.push(currentNode.right);
+    }
+  
+    return root;
+  };
+  
+  function swap(tree) {
+    [tree.left, tree.right] = [tree.right, tree.left];
+  }
+
+
+
+
 //recurisve approach
 
 // var invertTree = function(root) {
@@ -14,3 +39,4 @@
 //     [tree.left,tree.right]=[tree.right,tree.left]
 
 // }
+
