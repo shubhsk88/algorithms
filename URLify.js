f6ffb1f6fc2cae42b8664a89acfca428f086b5f8
Shubham Singh <shubham88singh@gmail.com>
2020-09-11 05:31:41 +0530

    Adding the solution of the validpalindromepermutation

---
+++ URLify.js
@@ -0,0 +1,17 @@
+function URLify(str,length)
+{
+    for(let i=0;i<=length;i++)
+    {
+        if(str[i]=" ")
+        {
+            str[i]="%20"
+        }
+
+        
+
+    }
+    for(let i=length;i<str.length;i++)
+    {
+
+    }
+}
